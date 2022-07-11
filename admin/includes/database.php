a1429a425a32bbc8766c544162b69204484b5876
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2022-07-11 11:07:27 -0400

    Initial vanilla php cms files

---
+++ admin/includes/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = mysqli_connect( 
+    "sql305.epizy.com", 
+    "epiz_31374598",
+    "uJ1oZFL7mN", 
+    "epiz_31374598_portfolio" 
+);
+
+mysqli_set_charset( $connect, 'UTF8' );
